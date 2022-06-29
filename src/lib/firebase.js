2d1ba03af6ea4e203d199d88d371e958b88f7648
hongnta <>
2022-06-29 16:21:58 +0000

    Use Firebase ::

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,20 +2,18 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBjlzSqBUKiPit4tOb5XvW1xnd_NORmrHE",
-  authDomain: "fir-sample-16ac1.firebaseapp.com",
-  projectId: "fir-sample-16ac1",
-  storageBucket: "fir-sample-16ac1.appspot.com",
-  messagingSenderId: "512462420994",
-  appId: "1:512462420994:web:2a6fe67efc2db45e8c3b1b"
+  apiKey: "AIzaSyAqt9HIjMZLKcGatloNC12f_Wc_7R2CcQ8",
+  authDomain: "fir-sample-4f3c6.firebaseapp.com",
+  projectId: "fir-sample-4f3c6",
+  storageBucket: "fir-sample-4f3c6.appspot.com",
+  messagingSenderId: "202286968628",
+  appId: "1:202286968628:web:b3c8fb37d9a7d35dad3f61"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig); 
+const app = initializeApp(firebaseConfig);
