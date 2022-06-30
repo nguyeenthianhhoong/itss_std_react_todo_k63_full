fa49d83c5d5c83751ee9e7874dc0dc9ad3a2a48f
hongnta <>
2022-06-30 07:21:44 +0000

    Hosting

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,13 +6,22 @@ import 'firebase/compat/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAqt9HIjMZLKcGatloNC12f_Wc_7R2CcQ8",
+//   authDomain: "fir-sample-4f3c6.firebaseapp.com",
+//   projectId: "fir-sample-4f3c6",
+//   storageBucket: "fir-sample-4f3c6.appspot.com",
+//   messagingSenderId: "202286968628",
+//   appId: "1:202286968628:web:b3c8fb37d9a7d35dad3f61"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAqt9HIjMZLKcGatloNC12f_Wc_7R2CcQ8",
-  authDomain: "fir-sample-4f3c6.firebaseapp.com",
-  projectId: "fir-sample-4f3c6",
-  storageBucket: "fir-sample-4f3c6.appspot.com",
-  messagingSenderId: "202286968628",
-  appId: "1:202286968628:web:b3c8fb37d9a7d35dad3f61"
+  apiKey: "AIzaSyAG5xDsv_I3W3sNAqInzBHOh7FSTFHZ6CE",
+  authDomain: "fir-2-8ac9a.firebaseapp.com",
+  projectId: "fir-2-8ac9a",
+  storageBucket: "fir-2-8ac9a.appspot.com",
+  messagingSenderId: "852986413487",
+  appId: "1:852986413487:web:50707b278c9a4ac315ef5a"
 };
 
 // Initialize Firebase
